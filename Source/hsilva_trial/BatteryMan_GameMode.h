e9a2392ebed335df3611f30cad57fb14fc35052c
Henry Silva Quevedo <Hsilva4495@eagle.fgcu.edu>
2020-09-29 15:24:58 -0400

    close
    1.12.00

--- Source/hsilva_trial/BatteryMan_GameMode.h
+++ Source/hsilva_trial/BatteryMan_GameMode.h
@@ -12,6 +12,32 @@
 UCLASS()
 class HSILVA_TRIAL_API ABatteryMan_GameMode : public AGameMode
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
+
+    ABatteryMan_GameMode();
+
+    virtual void BeginPlay() override; 
+    virtual void Tick(float DeltaTime) override; 
+
+    //this corresponds to pickup item
+    UPROPERTY(EditAnywhere)
+        TSubclassOf<APawn> PlayerRecharge; 
+
+    float Spawn_Z = 500.0f;
+
+    UPROPERTY(EditAnywhere)
+        float Spawn_X_Min; 
+
+    UPROPERTY(EditAnywhere)
+        float Spawn_X_Max;
+
+    UPROPERTY(EditAnywhere)
+        float Spawn_Y_Min;
+
+    UPROPERTY(EditAnywhere)
+        float Spawn_Y_Max;
+
+    void SpawnPlayerRecharge();   
+
 	
 };
